542ec26ca3f570fae6d5fa18ae79270c92f88d95
gereongoldbach <info@htrs.io>
2020-05-11 20:23:02 +0200

    Add subdirectory inc and the elementor-setup.php file

--- inc/elementor-setup.php
+++ inc/elementor-setup.php
@@ -6,7 +6,9 @@
  * @param $return
  * @return bool
  */
-function htrs_disable_page_title($return){
+function htrs_disable_page_title($return)
+{
     return false;
 }
+
 add_filter('hello_elementor_page_title', 'htrs_disable_page_title');
